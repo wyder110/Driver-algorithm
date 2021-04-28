1d26b6dc364dc27412d132dd5fabbed1902e1a6a
Arkadiusz <arkadiusz.cwikla98@gmail.com>
2021-04-28 21:12:55 +0200

    fix mutation

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import confLoader
 from startPopGen import generateFirstPopulation
 from objectiveFunction import objectiveFunction
 import crossover
+from mutation import *
 
 cities, packages, parameters = confLoader.confLoader("config/conf3.json")
 
@@ -10,19 +11,28 @@ cities, packages, parameters = confLoader.confLoader("config/conf3.json")
 pop = generateFirstPopulation(cities, packages, parameters, 100)
 sortingFun = lambda trace : objectiveFunction(cities, packages, trace)
 
-
+for p in pop[0]:
+    print(p)
+print()
+for i in range(4, len(pop[0])):
+    if can_be_deleted(pop[0], i):
+        print("deleted ", i)
+        deletion(pop[0], i)
+        break
+for p in pop[0]:
+    print(p)
 
 # cross = crossover.crossover(cities, packagesId, parameters, pop[0], pop[1])
 
-# ranking selection :O
-for _ in range(100):
-    pop.sort(key = sortingFun, reverse=True)
-    pop = pop[:20]
-    for i in range(len(pop)):
-        for j in range(i):
-            cross = crossover.crossover(cities, pop[i], pop[j])
-            pop.append(cross)
-            # pop.append(generateFirstPopulation(cities, packages, parameters, 1)[0])
+# # ranking selection :O
+# for _ in range(100):
+#     pop.sort(key = sortingFun, reverse=True)
+#     pop = pop[:20]
+#     for i in range(len(pop)):
+#         for j in range(i):
+#             cross = crossover.crossover(cities, pop[i], pop[j])
+#             pop.append(cross)
+#             # pop.append(generateFirstPopulation(cities, packages, parameters, 1)[0])
 
     
 
--- mutation.py
+++ mutation.py
@@ -1,11 +1,16 @@
-# top kek
+from confLoader import Configuration as Configuration
 
 # where, cities?
-def can_be_deleted(trace, index, connections):
-    # TODO
-    # if first then no
-    pass
-# where, cities?
+def can_be_deleted(trace, index):
+    if index < 1 or index > len(trace)-1:
+        return False
+    elif index == len(trace)-1:
+        return True
+    else:
+        previuos_step = trace[index-1]
+        currrent_step = trace[index]
+        next_step = trace[index+1]  # (from, to, packages)
+        return next_step[0] in list(Configuration.cities.get(previuos_step[0]).keys()) # connection beetwen cities
 
 
 def deletion(trace: list, index):
@@ -18,18 +23,47 @@ def deletion(trace: list, index):
         packages_delivered_to_current_city = list(set(previuos_step[2]) - set(currrent_step[2]))
 
         # removing "taken" packages until we reach "deleted" city
-        for i in range(index, len(trace)):
+        
+        for i in range(index+1, len(trace)):
             step = trace[i]
             if step[0] != currrent_step[0]: # different city
                 # remove "taken" packages
-                step[2] = [p for p in step[2] if p not in packages_taken_from_current_city]
+                packages = [p for p in step[2] if p not in packages_taken_from_current_city]
+                trace[i] = (step[0], step[1], packages)
             else:
                 break
-        minimal_space_left = 0
-        # TODO
+
+        # calculating free space in trailer
+        minimal_space_left = Configuration.parameters['maxCarry']
+        for i in range(index+1, len(trace)):
+            step = trace[i]
+            if step[0] != currrent_step[0]: # different city
+                weight = sum([Configuration.packagesId[p_id]['weigth'] for p_id in step[2]]) 
+                space_left = Configuration.parameters['maxCarry'] - weight
+                if space_left < minimal_space_left:
+                    minimal_space_left = space_left
+        
+        packages_to_add = []
+        for p in packages_delivered_to_current_city:
+            weight = Configuration.packagesId[p]['weigth']
+            if weight <= minimal_space_left:
+                minimal_space_left -= weight
+                packages_to_add.append(p)
+
+        if len(packages_to_add) > 0:
+            # adding "delivered" packages until we reach "deleted" city
+            for i in range(index+1, len(trace)):
+                step = trace[i]
+                if step[0] != currrent_step[0]: # different city
+                    # add "delivered" packages
+                    packages = step[2] + packages_to_add
+                    trace[i] = (step[0], step[1], packages)
+                else:
+                    break
+        trace[index-1] = (previuos_step[0], next_step[0], previuos_step[2])
+        trace.remove(currrent_step)
     else:  # delete from the end of trace
         trace.pop()
-    pass
 
 
 def check_and_delete():
