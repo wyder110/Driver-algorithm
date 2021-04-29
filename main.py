bb96e359a053ffb9eefe9e2f926a61289e54c62f
Arkadiusz <arkadiusz.cwikla98@gmail.com>
2021-04-29 03:02:46 +0200

    add new mutation

--- main.py
+++ main.py
@@ -3,51 +3,46 @@ from startPopGen import generateFirstPopulation
 from objectiveFunction import objectiveFunction
 import crossover
 from mutation import *
+from copy import copy
+from objectiveFunction import *
 
-cities, packages, parameters = confLoader.confLoader("config/conf3.json")
-
-
-
-pop = generateFirstPopulation(cities, packages, parameters, 100)
+cities, packages, parameters = confLoader.confLoader("config/conf.json")
 sortingFun = lambda trace : objectiveFunction(cities, packages, trace)
 
-for p in pop[0]:
-    print(p)
-print()
-for i in range(4, len(pop[0])):
-    if can_be_deleted(pop[0], i):
-        print("deleted ", i)
-        deletion(pop[0], i)
-        break
-for p in pop[0]:
-    print(p)
-
-# cross = crossover.crossover(cities, packagesId, parameters, pop[0], pop[1])
-
 # # ranking selection :O
-# for _ in range(100):
-#     pop.sort(key = sortingFun, reverse=True)
-#     pop = pop[:20]
-#     for i in range(len(pop)):
-#         for j in range(i):
-#             cross = crossover.crossover(cities, pop[i], pop[j])
-#             pop.append(cross)
-#             # pop.append(generateFirstPopulation(cities, packages, parameters, 1)[0])
+best_pop = []
+max_cash = 0
+for _ in range(50):
+    pop = generateFirstPopulation(cities, packages, parameters, 100)
+    for _ in range(100):
+        pop.sort(key = sortingFun, reverse=True)
+        pop = pop[:20]
+        for i in range(len(pop)):
+            trace_copy = copy(pop[i])
+            replacement(trace_copy)
+            pop.append(trace_copy)
+            # for j in range(i):
+            #     cross = crossover.crossover(cities, pop[i], pop[j])
+            #     pop.append(cross)
+            #     pop.append(generateFirstPopulation(cities, packages, parameters, 1)[0])
 
-    
+    pop.sort(key = sortingFun, reverse=True)
+    current_cash = sortingFun(pop[0])
+    print(sortingFun(pop[0]))
+    if current_cash > max_cash:
+        max_cash = current_cash
+        best_pop = pop[0]
 
+for p in best_pop:
+    print(p)
 
+cost = 0
+for current_city, next_city, trailer in best_pop:
+    distance = cities[current_city][next_city]
+    cost += 2*distance
 
-
-print(sortingFun(pop[0]))
-
-
-
-# for t in pop:
-#     print("Trace")
-#     for e in t:
-#         print(e)
-#     profit = objectiveFunction(cities, packages, t)
-#     print("profit:", profit)
-#     print()
-
+max_sum = sum([x['price'] for x in Configuration.packagesId.values()])
+print("max sum ",  max_sum)
+print("best sum ", max_cash)
+print("fuel cost ", cost)
+print("difference", max_sum-(max_cash+cost))
