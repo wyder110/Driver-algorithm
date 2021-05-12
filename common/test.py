20ce675e90fc4a07016cbe17d60aecddbf01ecc6
Arkadiusz <arkadiusz.cwikla98@gmail.com>
2021-05-13 00:46:32 +0200

    add bad removal testing

--- common/test.py
+++ common/test.py
@@ -1,5 +1,8 @@
+from functools import reduce
+from confLoader import Configuration as Configuration
+from common.objectiveFunction import *
 
-def checkIfTraceIsCorrect(trace):
+def checkTakenPackages(trace):
     taken = set()
     for i in range(1, len(trace)):
         takenInThisTurn = set(trace[i][2]) - set(trace[i-1][2])
@@ -17,3 +20,48 @@ def checkCitiesTrace(trace):
         if previous_step[1] != current_step[0]:
             return False
     return True
+
+def printObjFuncStepByStep(cities, packages, trace, max_sum):
+    profit = 0
+    total_fuel_cost = 0
+    trailer_old = []
+    for current_city, next_city, trailer in trace:
+        # packages profit
+        trailer_diff = [x for x in trailer_old if x not in trailer]
+        should_remove_in_current_city = [x for x in trailer_old if Configuration.packagesId[x]['to'] == current_city]
+        trailer_old = trailer
+        packages_info = getPackagesWithIds(packages, trailer_diff)
+        print(current_city,"->",next_city, "\n\t trailer:", trailer)
+        print("\t current profit:", profit)
+        print("\t removed:", trailer_diff, "\n\t should remove:", should_remove_in_current_city)
+        if set(should_remove_in_current_city) != set(trailer_diff):
+            print("BAD REMOVAL")
+
+        for p in packages_info:
+            profit += p["price"]
+            print("\t package: ", p["id"], " price: ", p["price"])
+        print("\t after delivery profit:", profit)
+        # fuel cost
+        distance = cities[current_city][next_city]
+        fuel_cost = fuel(distance)
+        print("\t fuel cost:", fuel_cost)
+        profit -= fuel_cost
+        total_fuel_cost += fuel_cost
+        print("\t after fuel profit:", profit)
+        print("\t total fuel cost:", total_fuel_cost)
+        print("\t free profit:", profit+total_fuel_cost)
+        print("\t max free profit - free profit:", max_sum - (profit+total_fuel_cost))
+    
+    return profit
+
+
+def checkBadRemoval(trace):
+    trailer_old = []
+    for current_city, _, trailer in trace:
+        trailer_diff = [x for x in trailer_old if x not in trailer]
+        should_remove_in_current_city = [x for x in trailer_old if Configuration.packagesId[x]['to'] == current_city]
+        trailer_old = trailer
+        if set(should_remove_in_current_city) != set(trailer_diff):
+            return True
+    
+    return False
--- geneticMain.py
+++ geneticMain.py
@@ -4,14 +4,16 @@ import confLoader
 import genetic.crossover as crossover
 import genetic.mutation as mutation
 from common.objectiveFunction import *
+from common.test import *
 from genetic.selection import *
 from genetic.startPopGen import generateFirstPopulation
+import time
 
-cities, packages, parameters = confLoader.confLoader("config/conf.json")
+cities, packages, parameters = confLoader.confLoader("config/conf3.json")
 
 population_count = 100
-number_of_tests = 1000
-number_of_iterations = 1000
+number_of_tests = 100
+number_of_iterations = 100
 selection_type = 0 # 0 - ranking, 1 - tournament, 2 - roulette
 selected_percent = 0.2
 
@@ -19,17 +21,19 @@ max_sum = sum([x['price'] for x in Configuration.packagesId.values()])
 
 best_pop = []
 max_cash = 0
-for _ in range(number_of_tests):
+# start = time.time()
+for number_test in range(number_of_tests):
+    print(number_test)
     pop = generateFirstPopulation(cities, packages, parameters, population_count)
     for _ in range(number_of_iterations):
         pop = ranking_selection(pop, int(population_count*selected_percent))
         for i in range(len(pop)):
-            trace_copy = copy(pop[i])
-            mutation.replacement(trace_copy)
-            pop.append(trace_copy)
-            # for j in range(i):
-                # cross = crossover.crossover(cities, pop[i], pop[j])
-                # pop.append(cross)
+            # trace_copy = copy(pop[i])
+            # mutation.replacement(trace_copy)
+            # pop.append(trace_copy)
+            for j in range(i):
+                cross = crossover.crossover(cities, pop[i], pop[j])
+                pop.append(cross)
                 # pop.append(generateFirstPopulation(cities, packages, parameters, 1)[0])
 
     pop.sort(key=sortingFun, reverse=True)
@@ -38,7 +42,7 @@ for _ in range(number_of_tests):
     #     print(p)
     # print(current_cash)
     cost = 0
-    for current_city, next_city, trailer in best_pop:
+    for current_city, next_city, trailer in pop[0]:
         distance = cities[current_city][next_city]
         cost += 2*distance
     if max_sum-(current_cash+cost) < 0:
@@ -46,10 +50,20 @@ for _ in range(number_of_tests):
             print(p)
         print(current_cash)
         print("difference", max_sum-(current_cash+cost))
+    if checkBadRemoval(pop[0]):    
+        printObjFuncStepByStep(cities, packages, pop[0], max_sum)
+    # if not checkCitiesTrace(pop[0]) or not checkTakenPackages(pop[0]):
+    #     print("WRONG TRACE")
+    #     for p in pop[0]:
+    #         print(p)
+
     if current_cash > max_cash:
         max_cash = current_cash
         best_pop = pop[0]
+        printObjFuncStepByStep(cities, packages, pop[0], max_sum)
 
+# stoptime = time.time()
+# print("Elapsed time: ", stoptime-start)
 for p in best_pop:
     print(p)
 
