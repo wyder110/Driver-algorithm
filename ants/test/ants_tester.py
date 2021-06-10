3dfa8f25ca3aeead8da62eb5f5dbb14bb154313a
Arkadiusz <arkadiusz.cwikla98@gmail.com>
2021-06-10 22:01:58 +0200

    add ants tests

---
+++ ants/test/ants_tester.py
@@ -0,0 +1,78 @@
+
+import time
+from ants.pheromones import createPheromonesMap, updatePheromoneMap
+from ants.ants import antTakeLeavePackages, newAnt, nextStep
+from common.objectiveFunction import *
+from config.confLoader import *
+
+
+def ants_driver(config_path="config/conf3.json", number_of_tests=100, number_of_iterations=100, starting_population=10, alpha=0.8, beta=0.5, ro=0.01):
+    print_start_info(config_path, number_of_tests, number_of_iterations, starting_population, alpha, beta, ro)
+
+    cities, _, _ = confLoader(config_path)
+
+    max_sum = sum([x['price'] for x in Configuration.packagesId.values()])
+    max_steps = Configuration.parameters["maxSteps"]
+    startingCity = Configuration.parameters["start"]
+
+    max_cash = 0
+    max_ants_count = 0
+
+    startTime = time.time()
+
+    for _ in range(number_of_tests):
+        pheromoneMap = createPheromonesMap(cities)
+        ants = []
+
+        for _ in range(starting_population):
+            ants.append(newAnt(startingCity, pheromoneMap, alpha, beta))
+
+        for _ in range(number_of_iterations):
+            ants.append(newAnt(startingCity, pheromoneMap, alpha, beta))
+
+            for ant in ants:
+                if len(ant) < max_steps:
+                    cityFrom = ant[-1][1]
+                    trailer = ant[-1][2]
+                    nextCity = nextStep(
+                        pheromoneMap, cityFrom, trailer, alpha, beta)
+                    newPackages = antTakeLeavePackages(ant)
+                    ant.append((cityFrom, nextCity, newPackages))
+
+            updatePheromoneMap(pheromoneMap, ants, ro)
+
+        def sortingFun(trace): return objectiveFunction(
+            Configuration.cities, Configuration.packages, trace)
+        ants.sort(key=sortingFun, reverse=True)
+        best_ants = ants[0]
+        current_best_cash = sortingFun(best_ants)
+        if current_best_cash > max_cash:
+            max_cash = current_best_cash
+            max_ants_count = len(best_ants)
+
+    stoptime = time.time()
+    elapsed_time = stoptime - startTime
+
+    print_end_info(max_sum, max_cash, elapsed_time, max_ants_count)
+
+    return max_sum, max_cash, elapsed_time, max_ants_count
+
+
+def print_end_info(max_sum, max_cash, elapsed_time, max_ants_count):
+    print("test ended with results: ")
+    print("\t max_sum:", max_sum)
+    print("\t max_cash:", max_cash)
+    print("\t elapsed_time:", elapsed_time)
+    print("\t max_ants_count:", max_ants_count)
+    print("- - - - - - - - - - - -")
+
+
+def print_start_info(config_path, number_of_tests, number_of_iterations, starting_population, alpha, beta, ro):
+    print("starting test with parameters: ")
+    print("\t config_path:", config_path)
+    print("\t number_of_tests:", number_of_tests)
+    print("\t number_of_iterations:", number_of_iterations)
+    print("\t starting_population:", starting_population)
+    print("\t alpha:", alpha)
+    print("\t beta:", beta)
+    print("\t ro:", ro)
---
+++ ants_test.py
@@ -0,0 +1,36 @@
+import pandas
+import ants.test.ants_tester as ants_tester
+
+input_file = 'ants/test/ants_input.csv'
+output_file = "ants/test/ants_output.csv"
+
+df = pandas.read_csv(input_file)
+
+number_of_tests = df['id'].max()
+
+df_out = pandas.DataFrame(
+    columns=['id', 'max_sum', 'max_cash', 'elapsed_time', 'max_ants_count'])
+
+for test in range(number_of_tests):
+    current_id = test+1
+    current_df = df[df['id'] == current_id]
+    for row in range(len(current_df)):
+        current_row = df.iloc[row]
+        max_sum, max_cash, elapsed_time, max_ants_count = ants_tester.ants_driver(
+            config_path="config/conf3.json",
+            number_of_tests=current_row.loc['number_of_tests'], 
+            number_of_iterations=current_row.loc['number_of_iterations'], 
+            starting_population=current_row.loc['starting_population'], 
+            alpha=current_row.loc['alpha'], 
+            beta=current_row.loc['beta'], 
+            ro=current_row.loc['ro']
+        )
+        df_out = df_out.append(pandas.DataFrame({
+            'id': [current_id],
+            'max_sum': [max_sum],
+            'max_cash': [max_cash],
+            'elapsed_time': [elapsed_time], 
+            'max_ants_count': [max_ants_count]}), 
+            ignore_index=True)
+    
+    df_out.to_csv(output_file, index=False)
